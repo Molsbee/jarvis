0a2cf8a99545eed3be03a20ca75d5ca6d5cc2d2e
molsbee <molsbee@gmail.com>
2020-07-06 08:17:42 -0500

    Changed formatting of IPAddress slightly.

--- model/clc.go
+++ model/clc.go
@@ -15,12 +15,13 @@ type IPAddress struct {
 }
 
 func (i IPAddress) String() string {
-	return fmt.Sprintf(`Address:       %s
-Location:      %s
-ID:            %d
-NetworkID:     %d
-HardwareUUID:  %s
-IsPublic:      %v
-IsClaimed:     %v
-IsClaimable:   %v`, i.Address, i.Location, i.ID, i.NetworkID, i.HardwareUUID, i.IsPublic, i.IsClaimed, i.IsClaimable)
+	return fmt.Sprintf(
+		`	Address:       %s
+	Location:      %s
+	ID:            %d
+	NetworkID:     %d
+	HardwareUUID:  %s
+	IsPublic:      %v
+	IsClaimed:     %v
+	IsClaimable:   %v`, i.Address, i.Location, i.ID, i.NetworkID, i.HardwareUUID, i.IsPublic, i.IsClaimed, i.IsClaimable)
 }
