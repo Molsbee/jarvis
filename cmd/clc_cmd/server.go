2a587da5171f62b38b17e32db4290f53ac6459ff
molsbee <molsbee@gmail.com>
2020-08-04 12:50:22 -0500

    Changed command to simplify it and remove flags.

--- cmd/clc_cmd/server.go
+++ cmd/clc_cmd/server.go
@@ -4,50 +4,53 @@ import (
 	"fmt"
 	"github.com/Molsbee/jarvis/config"
 	"github.com/Molsbee/jarvis/service/clc"
+	"github.com/google/uuid"
 	"github.com/spf13/cobra"
 	"os"
+	"strings"
 )
 
 var (
-	serverName   string
-	hardwareUUID string
-	environment  string
+	environment string
 )
 
 func vm() *cobra.Command {
 	vm := &cobra.Command{
 		Use:     "vm",
-		Example: "vm --serverName UC1T3NMT2016-01 --env prod",
+		Example: "vm [server name or hardware uuid] --env prod",
 		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) == 0 {
+				fmt.Println("please provide either a server name or hardware uuid")
+				os.Exit(1)
+			}
+
 			environment, err := config.GetEnvironment(environment)
 			if err != nil {
 				fmt.Println(err)
 				os.Exit(1)
 			}
 
-			// TODO: Could move from persistent flags and perform REGEX to determine if the server name is UUID.
-			if len(serverName) != 0 {
-				server, err := clc.GetServerDetails(environment, serverName)
-				if err != nil {
-					fmt.Println(err)
-					os.Exit(1)
-				}
-				fmt.Println(server)
-			} else if len(hardwareUUID) != 0 {
-				server, err := clc.GetServerDetailsByHardwareUUID(environment, hardwareUUID)
+			serverName := args[0]
+			hardwareUUID, parseErr := uuid.Parse(serverName)
+			if parseErr == nil {
+				nakedUUID := strings.ToLower(fmt.Sprintf("%X%X%X%X%X", hardwareUUID[0:4], hardwareUUID[4:6], hardwareUUID[6:8], hardwareUUID[8:10], hardwareUUID[10:]))
+				server, err := clc.GetServerDetailsByHardwareUUID(environment, nakedUUID)
 				if err != nil {
 					fmt.Println(err)
 					os.Exit(1)
 				}
 				fmt.Println(server)
 			} else {
-				fmt.Println("please provide either a server name or hardware uuid")
-				os.Exit(1)
+				server, err := clc.GetServerDetails(environment, serverName)
+				if err != nil {
+					fmt.Println(err)
+					os.Exit(1)
+				}
+				fmt.Println(server)
 			}
 		},
 	}
-	vm.PersistentFlags().StringVarP(&serverName, "serverName", "s", "", "--serverName UC1T3NMT2016-01")
-	vm.PersistentFlags().StringVarP(&hardwareUUID, "hardwareUUID", "u", "", "--hardwareUUID 8f295b9c7122487ab0b62c6a9b523ec2")
+
 	vm.PersistentFlags().StringVarP(&environment, "env", "e", "prod", "--env dev (defaults to prod)")
 	return vm
 }
