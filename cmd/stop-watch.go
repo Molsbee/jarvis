cfdee527929dd4aba3ffbadf0423ccf499f8a230
molsbee <molsbee@gmail.com>
2020-09-01 14:19:44 -0500

    Simplified hacker-news command for top best new. Created basic stop watch command which calls out to say to make beep looking at replacing with an audio library to play a sound.

---
+++ cmd/stop-watch.go
@@ -0,0 +1,36 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+	"os/exec"
+	"time"
+)
+
+var stopWatch = &cobra.Command{
+	Use: "stop-watch",
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			fmt.Println("please provide a valid duration for the stop watch (ex. 300s, 1m) that is greater than 1 second")
+			os.Exit(1)
+		}
+
+		duration, err := time.ParseDuration(args[0])
+		if err != nil || duration.Seconds() < 1 {
+			fmt.Println("please provide a valid duration for the stop watch (ex. 300s, 1m) that is greater that 1 second")
+			os.Exit(1)
+		}
+
+		for i := 0; i < int(duration.Seconds()); i++ {
+			if i != 0 && i%60 == 0 {
+				fmt.Println()
+			}
+
+			fmt.Print("|")
+			time.Sleep(1 * time.Second)
+		}
+		fmt.Printf("\nTimer has completed\n")
+		exec.Command("say", "beep beep beep beep beep beep").Run()
+	},
+}
